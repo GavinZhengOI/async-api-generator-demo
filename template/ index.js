b26b8279a25d45da86c0491f2102680c8051bac3
GavinZhengOI <33168669+GavinZhengOI@users.noreply.github.com>
2024-03-21 01:19:36 -0400

    5c. Creating a reusable component

--- template/ index.js
+++ template/ index.js
@@ -1,23 +1,20 @@
-import { File } from '@asyncapi/generator-react-sdk';
-
-// notice that now the template not only gets the instance of parsed AsyncAPI document but also the parameters
+// 1
+import { File, Text } from '@asyncapi/generator-react-sdk'
 export default function ({ asyncapi, params }) {
- 
   return (
     <File name="client.py">
-      {`import paho.mqtt.client as mqtt
 
-mqttBroker = "${asyncapi.servers().get(params.server).url()}"
+      <Text newLines={2}>import paho.mqtt.client as mqtt</Text>
 
-class TemperatureServiceClient:
-    def __init__(self):
-        self.client = mqtt.Client()
-        self.client.connect(mqttBroker)
+      <Text newLines={2}>mqttBroker = "{asyncapi.servers().get(params.server).url()}"</Text>
 
+      <Text newLines={2}>class {asyncapi.info().title().replaceAll(' ', '')}Client:</Text>
 
-    def sendTemperatureChange(self, id):
-        topic = "temperature/changed"
-        self.client.publish(topic, id)`}
-    </File>
+      <Text indent={2} newLines={2}>
+        {`def __init__(self):
+            self.client = mqtt.Client()
+            self.client.connect(mqttBroker)`}
+      </Text>
+      </File>
   )
 }
