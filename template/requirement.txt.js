2f3f66edc144109454382417af4765fe2e9284a2
GavinZhengOI <33168669+GavinZhengOI@users.noreply.github.com>
2024-03-30 16:01:42 -0400

    Update requirement.txt file name

--- template/requirement.txt.js
+++ template/requirement.txt.js
@@ -2,7 +2,7 @@ import { File, Text } from '@asyncapi/generator-react-sdk'
 
 export default function requirementList({ asyncapi, params }){
     return (
-        <File name="requirement.txt">
+        <File name="requirements.txt">
             <Text>paho-mqtt==1.6.1</Text>
         </File>
     )
